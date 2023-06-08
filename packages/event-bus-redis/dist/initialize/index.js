8ce4b353e1ddb4597b5a772fd22ea0f1bd0c7f08
Jueun Grace Yun <graceyunn@gmail.com>
2023-06-08 19:57:22 -0400

    push up dist folders

---
+++ packages/event-bus-redis/dist/initialize/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.initialize = void 0;
+const modules_sdk_1 = require("@medusajs/modules-sdk");
+const initialize = async (options) => {
+    const serviceKey = modules_sdk_1.Modules.EVENT_BUS;
+    const loaded = await modules_sdk_1.MedusaModule.bootstrap(serviceKey, "@medusajs/event-bus-redis", options, undefined);
+    return loaded[serviceKey];
+};
+exports.initialize = initialize;
+//# sourceMappingURL=index.js.map
